044c245a2f702c6909b87233923a571b80bc6dfb
Nikhil Singh <nikhil.singh@olx.com>
2020-04-20 02:45:16 +0530

    Added User Details icon and minor ui changes

--- app/src/main/java/com/example/presence/UserDetailsActivity.kt
+++ app/src/main/java/com/example/presence/UserDetailsActivity.kt
@@ -1,12 +1,16 @@
 package com.example.presence
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class UserDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_details)
+
+        supportActionBar?.hide()
+
     }
 }
