d816a3207e84252f515abdffaa225f096bb17405
Nikhil Singh <nikhil.singh@olx.com>
2020-04-21 21:07:55 +0530

    Added StartupDialog

--- app/src/main/java/com/example/presence/UserDetailsActivity.kt
+++ app/src/main/java/com/example/presence/UserDetailsActivity.kt
@@ -3,10 +3,13 @@ package com.example.presence
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.presence.widgets.StartupInformationDialog
 import kotlinx.android.synthetic.main.activity_user_details.*
 
 
 class UserDetailsActivity : AppCompatActivity() {
+
+    private lateinit var startupDialog: StartupInformationDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +17,19 @@ class UserDetailsActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        openStartupDialog()
+
         btnAddSubjects.setOnClickListener {
             val addSubjectsActivity = Intent(this, AddSubjectsActivity::class.java)
             startActivity(addSubjectsActivity)
         }
+    }
+
+    fun openStartupDialog(){
+        startupDialog = StartupInformationDialog()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack(null)
+        startupDialog.show(fragmentTransaction,
+            resources.getString(R.string.label_start_up_dialog_tag))
     }
 }
---
+++ app/src/main/java/com/example/presence/widgets/StartupInformationDialog.kt
@@ -0,0 +1,33 @@
+package com.example.presence.widgets
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.example.presence.R
+
+class StartupInformationDialog: DialogFragment() {
+
+    private lateinit var startupDialog: AlertDialog.Builder
+    private lateinit var btnOk: Button
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        var layoutInflater: LayoutInflater? = activity?.layoutInflater
+        val view = layoutInflater?.inflate(R.layout.startup_dialog_layout, null)
+
+        startupDialog = AlertDialog.Builder(activity)
+        startupDialog.setView(view)
+
+        btnOk = view?.findViewById(R.id.btnStartupDialogOk)!!
+
+        btnOk.setOnClickListener {
+            dismiss()
+        }
+
+        return startupDialog.create()
+    }
+
+}
